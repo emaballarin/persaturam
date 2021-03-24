904e1cd1b7fc0c88f0e2a10e2805c136d6351284
Emanuele Ballarin <emanuele@ballarin.cc>
2021-03-25 00:43:59 +0100

    Update ryou.sh

--- scripts/ryou.sh
+++ scripts/ryou.sh
@@ -1,15 +1,17 @@
+#!/usr/bin/env zsh
+
 #######################################################
 ##            RYOU - Roll Your Own Unison            ##
-## (C) 2019 Emanuele Ballarin <emanuele@ballarin.cc> ##
+## (C) 2021 Emanuele Ballarin <emanuele@ballarin.cc> ##
 ##             Distribution: MIT License             ##
 #######################################################
 
 ################
 ### SETTINGS ###
 ################
-SSH_USERNAME="..."
-SSH_REMOTE="..."
-SSH_DESTINATION="..."
+SSH_USERNAME=" "
+SSH_REMOTE=" "
+SSH_DESTINATION=" "
 
 ################################################################################
 
@@ -29,10 +31,10 @@ cp $(realpath $(which unison-fsmonitor)) ./
 ln -s ./unison-text ./unison
 
 # Patch
-patchelf --set-rpath "$SSH_DESTINATION/ryou" unison-text
-patchelf --set-rpath "$SSH_DESTINATION/ryou" unison-fsmonitor
-patchelf --set-interpreter "$SSH_DESTINATION/ryou"/ld-linux-x86-64.so.2 unison-text
-patchelf --set-interpreter "$SSH_DESTINATION/ryou"/ld-linux-x86-64.so.2 unison-fsmonitor
+patchelf --set-rpath "$SSH_DESTINATION" unison-text
+patchelf --set-rpath "$SSH_DESTINATION" unison-fsmonitor
+patchelf --set-interpreter "$SSH_DESTINATION"/ld-linux-x86-64.so.2 unison-text
+patchelf --set-interpreter "$SSH_DESTINATION"/ld-linux-x86-64.so.2 unison-fsmonitor
 
 # Copy libraries
 cp /usr/lib/libutil.so.1 /usr/lib/libm.so.6 /usr/lib/libdl.so.2 /usr/lib/libc.so.6 /lib64/ld-linux-x86-64.so.2 ./
